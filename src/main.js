93805a757095fc5feab635fb1a3aa5087977d09e
AlongkornFR <144777531+AlongkornFR@users.noreply.github.com>
2024-03-25 12:37:13 +0100

    landibg p
    age

--- src/main.js
+++ src/main.js
@@ -3,3 +3,13 @@ import { Application } from '@splinetool/runtime';
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
 app.load('https://prod.spline.design/33JVBKmHMCb0cZ98/scene.splinecode');
+
+document.addEventListener("DOMContentLoaded", function() {
+    var divClicable = document.getElementById("creatTopic");
+
+    // Ajoute un écouteur d'événements pour le clic sur l'élément div
+    divClicable.addEventListener("click", function() {
+        // Redirige l'utilisateur vers la nouvelle page
+        window.location.href = "creaTopic.html";
+    });
+});
